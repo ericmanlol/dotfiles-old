d2a50f79790123c70693ebfe0d1dd4fcde7702b3
Eric Man <lol.ericman@gmail.com>
2014-04-14 12:56:48 -0400

    re-compiled vim from source after system upgraded. vim7.4 atm

--- buildvim.sh
+++ buildvim.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-./configure \
---prefix=/usr/local/bin \
---with-features=huge \
---with-compiledby="woo" \
---enable-multibyte \
---enable-gui=gtk2 \
---enable-rubyinterp \
---enable-pythoninterp \
---with-python-config-dir=/usr/local/lib/python2.7/config \
-#--enable-pythoninterp \
-#--with-python-config-dir=/usr/local/lib/python2.7/config \
---enable-luainterp
-#--with-lua-prefix=/usr --with-luajit \
-#--enable-gpm \
+# ./configure \
+# --prefix=/usr/local/bin \
+# --with-features=huge \
+# --with-compiledby="woo" \
+# --enable-multibyte \
+# --enable-gui=gtk2 \
+# --enable-rubyinterp \
+# --enable-pythoninterp \
+# --with-python-config-dir=/usr/local/lib/python2.7/config \
+# #--enable-pythoninterp \
+# #--with-python-config-dir=/usr/local/lib/python2.7/config \
+# --enable-luainterp
+# #--with-lua-prefix=/usr --with-luajit \
+# #--enable-gpm \
 #--enable-cscope \
 #--enable-fontset \
 #--enable-fail-if-missing
+./configure --prefix=/usr/local/bin --with-features=huge --with-compiledby="woo" --enable-multibyte \
+--enable-gui=gtk2 --enable-rubyinterp --enable-pythoninterp --enable-python3interp --enable-luainterp 
