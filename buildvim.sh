bfa0ed42cbccbda93c91b585d112ea2de3f0ec9a
Eric Man <lol.ericman@gmail.com>
2014-04-15 17:36:39 -0400

    Update buildvim.sh

--- buildvim.sh
+++ buildvim.sh
@@ -17,5 +17,8 @@
 #--enable-cscope \
 #--enable-fontset \
 #--enable-fail-if-missing
+
+
+#current for 7.4 -- 4-15-2014
 ./configure --prefix=/usr/local/bin --with-features=huge --with-compiledby="woo" --enable-multibyte \
 --enable-gui=gtk2 --enable-rubyinterp --enable-pythoninterp --enable-python3interp --enable-luainterp 
